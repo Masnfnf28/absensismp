edefdc7f3f4bbf83d7faffc90e5091d9eeb278c1
Masnfnf28 <dimasnfnf@gmail.com>
2025-07-19 13:35:33 +0700

    first

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Http\Controllers\DatasiswaController;
+use App\Http\Controllers\ProfileController;
+use App\Models\Datasiswa;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+
+Route::resource('datasiswa', DatasiswaController::class)->middleware('auth');
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';
